79e6295d7f2c66a213f52b74252822d2b294bb06
Sahil <sahilsharma2000dtc@gmail.com>
2024-09-29 19:40:49 +0530

    setup the grahql server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
-import express from 'express'
-import { ApolloServer } from '@apollo/server';
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 
-const app = express();
-const PORT =  process.env.PORT || 8080;
+//init functio for graphqlserver
+async function init() {
+  const app = express();
+  const PORT = process.env.PORT || 8080;
+  app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({message: "Server is up and running..."})
-})
+  //creating graphqlserver
+  const glServer = new ApolloServer({
+    typeDefs: `
+        type Query {
+            hello: String,
+            say(name: String): String
+        }
+    `,//schema
+    resolvers: {
+        Query:{
+            hello: () => `Hey There i am graphql server`,
+            say:(_,{name}:{name:string}) => `Hey, ${name} how are you?` 
+        }
+    },//actual logic and function
+  });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
+  await glServer.start();
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running..." });
+
+  });
+
+  app.use("/graphql", expressMiddleware(glServer));
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+init();
